aac7eb0b26832ddd9a73a0931998ad3c0b064f76
Elliot Bobrow <elliotgreybobrow@gmail.com>
2021-03-25 18:21:26 -0700

    Add routes and controllers to template

--- template/src/index.ts
+++ template/src/index.ts
@@ -1,12 +1,14 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+
+import homeRoutes from './routes';
+import authRoutes from './routes/auth';
 
 const app = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello world');
-});
+app.use('/', homeRoutes);
+app.use('/auth', authRoutes);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`App listening on port ${PORT}`);
+	console.log(`App listening on port ${PORT}`);
 });
